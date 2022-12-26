22f06698feed519cf4d2363896af29cc98609fc7
Dartar <07kirgame@gmail.com>
2022-12-26 13:14:12 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule)
 
     app.enableCors({
-        origin: ['http://localhost:5000/', 'http://188.225.87.70:5000/', 'http://127.0.0.1:5000/'],
+        origin: [
+            'http://localhost:5000/',
+            'http://188.225.87.70:5000/',
+            'http://127.0.0.1:5000/',
+            'http://localhost:3000',
+            'http://188.225.87.70:3000',
+        ],
         credentials: true,
     })
 
